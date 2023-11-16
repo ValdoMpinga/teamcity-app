2f00422c9014dbf120bb75b5597cdaf5b81affc7
Valdo Mpinga <valdompinga2@gmail.com>
2023-11-16 18:33:33 +0000

    log test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // index.js
-const app = require('./calculator');
+// const app = require('./calculator');
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>
-{
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () =>
+// {
+//     console.log(`Server is running on port ${PORT}`);
+// });
  
+console.log('aaa')
